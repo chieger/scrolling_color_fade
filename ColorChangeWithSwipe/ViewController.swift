974b8b0fe7e425f2ad55a5282b03a788ebd40a45
Charlie Hieger <charlie@codepath.com>
2016-10-26 09:57:38 -0700

    Initial Commit

---
+++ ColorChangeWithSwipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorChangeWithSwipe
+//
+//  Created by Charlie Hieger on 10/26/16.
+//  Copyright © 2016 Charlie Hieger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
