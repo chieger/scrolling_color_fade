8f80713852e0b5a33b45c4cd7d5e0d241ee55de4
Charlie Hieger <charlie@codepath.com>
2016-10-26 11:33:08 -0700

    initial setup with color change on scroll

---
+++ ColorChangeWithSwipe/WelcomeViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  ColorChangeWithSwipe
+//
+//  Created by Charlie Hieger on 10/26/16.
+//  Copyright © 2016 Charlie Hieger. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    @IBOutlet weak var pageControl: UIPageControl!
+    var scrollViewContentOffsetMaxX: CGFloat!
+    var numberOfWelcomeScreens: Int!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.delegate = self
+        numberOfWelcomeScreens = 6
+        let contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(numberOfWelcomeScreens), height: scrollView.frame.size.height)
+        scrollView.contentSize = contentSize
+        scrollViewContentOffsetMaxX = scrollView.contentSize.width - scrollView.frame.size.width
+        scrollView.backgroundColor = UIColor(hue: 0, saturation: 1, brightness: 1, alpha: 1)
+
+        for screenNumber in 0..<numberOfWelcomeScreens {
+            let label = UILabel()
+            label.text = String(screenNumber + 1)
+            label.textColor = UIColor.white
+            label.font = UIFont.systemFont(ofSize: 200, weight: UIFontWeightThin)
+            label.sizeToFit()
+            scrollView.addSubview(label)
+            let width = scrollView.frame.size.width
+            label.center = CGPoint(x: width / 2 + CGFloat(screenNumber) * width, y: scrollView.frame.size.height / 2)
+
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+
+}
+
+extension WelcomeViewController: UIScrollViewDelegate {
+    func color(for offset: CGFloat) -> UIColor {
+        let hue = offset / scrollViewContentOffsetMaxX
+        let color = UIColor(hue: hue, saturation: 1, brightness: 1, alpha: 1)
+        return color
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print("OffsetX: \(scrollView.contentOffset.x), MaxOffsetX: \(scrollViewContentOffsetMaxX)")
+        let offset = scrollView.contentOffset.x
+        scrollView.backgroundColor = color(for: offset)
+    }
+}
+
